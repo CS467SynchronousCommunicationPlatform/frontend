97aa3d8e60fb3ebc0c9e03701845188d37f25124
Blake Hakkila <hakkilab@oregonstate.edu>
2024-10-24 18:36:40 -0700

    url processing based on supabase redirect example

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -12,13 +12,27 @@ import { redirect } from 'next/navigation'
 
 import { createClient } from '@/utils/supabase/server'
 
+/*
+From Supabase docs on authentication redirects (https://supabase.com/docs/guides/auth/redirect-urls#vercel-preview-urls)
+*/
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    'http://localhost:3000/'
+  // Make sure to include `https://` when not localhost.
+  url = url.startsWith('http') ? url : `https://${url}`
+  // Make sure to include a trailing `/`.
+  url = url.endsWith('/') ? url : `${url}/`
+  return url
+}
+
 export async function login() {
   const supabase = await createClient()
-  const origin = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo: `${origin}/auth/callback`,
+      redirectTo: `${getURL()}/auth/callback`,
     },
   })
 
