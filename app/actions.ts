e48a8c3ba88eb7692bbd6b0797ec7dce2168d3e5
Arberim Ame <amea@oregonstate.edu>
2024-10-26 01:44:38 -0400

    Add isDevEnv check

--- app/actions.ts
+++ app/actions.ts
@@ -2,18 +2,20 @@
 
 import { createClient } from "@supabase/supabase-js"
 
+// Set dev URL or production URL to redirect to
+const isDevEnv = process.env.NEXT_PUBLIC_ENV === 'development';
+const urlRedirect = isDevEnv ? 'http://localhost:3000/chat' : 'https://frontend-self-tau.vercel.app/'
+
 const googleOauth = async () => {
     const supabase = await createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
 
     await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `http://localhost:3000/chat`,
+          redirectTo: urlRedirect,
         },
       })
 }
 
 export default googleOauth;
-
-
   
