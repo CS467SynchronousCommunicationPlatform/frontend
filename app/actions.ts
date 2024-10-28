b583885e9d5a50f7fffcad71d3fb2a1dca411447
Arberim Ame <amea@oregonstate.edu>
2024-10-27 20:24:34 -0400

    Fix urlRedirect

--- app/actions.ts
+++ app/actions.ts
@@ -4,7 +4,7 @@ import { createClient } from "@supabase/supabase-js"
 
 // Set dev URL or production URL to redirect to
 const isDevEnv = process.env.NEXT_PUBLIC_ENV === 'development';
-const urlRedirect = isDevEnv ? 'http://localhost:3000/chat' : 'https://frontend-self-tau.vercel.app/'
+const urlRedirect = isDevEnv ? 'http://localhost:3000/chat' : 'https://frontend-self-tau.vercel.app/chat'
 
 const googleOauth = async () => {
     const supabase = await createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
