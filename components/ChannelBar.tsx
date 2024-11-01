2c527473dbd9d3aa93eaeeef3ba525df82ae5037
Scottie Thompson <scottiecodes@gmail.com>
2024-10-31 23:28:30 -0400

    Componetize Left Side Bar (Channel Bar)

---
+++ components/ChannelBar.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './ChatPage.module.css';
+import { signout } from '@/app/login/actions';
+
+const ChannelBar: React.FC = () => (
+    <div className={styles.sidebarLeft}>
+        <h3>Channels</h3>
+        <ul>
+            <li className={styles.activeChannel}>general</li>
+            <li>User3</li>
+            <li>User4</li>
+            <li>User7</li>
+        </ul>
+        <button onClick={signout} className={styles.logoutButton}>Logout</button>
+    </div>
+);
+
+export default ChannelBar;
