52ce07b51106ad0cc685c4195449b2b51d98228b
Arberim Ame <amea@oregonstate.edu>
2024-11-07 00:12:58 -0500

    Rename from Chat to PreviousMessages, remove dummy message data

---
+++ components/PreviousMessages.tsx
@@ -0,0 +1,19 @@
+// components/MessageList.tsx
+import React from 'react';
+import Message, { MessageProps } from '@/components/Message';
+import styles from '@/app/ChatPage.module.css';
+
+export default function PreviousMessages({ messages }: { messages: MessageProps[] }) {
+    return (
+        <div className={styles.chatWindow}>
+            {messages.map((message, index) => (
+                <Message
+                    key={index}
+                    user={message.user}
+                    timestamp={message.timestamp}
+                    body={message.body}
+                />
+            ))}
+        </div>
+    )
+}
