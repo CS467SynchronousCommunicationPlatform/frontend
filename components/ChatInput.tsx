8c50b4d4817efaff24b3c9a3a5d904aded2f1fc4
Arberim Ame <amea@oregonstate.edu>
2024-11-07 00:09:40 -0500

    Add ChatInputProps, add submit and onChange handlers

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,13 +1,26 @@
 // components/MainChatSection.tsx
 import React from 'react';
-import styles from '../app/ChatPage.module.css';
+import styles from '@/app/ChatPage.module.css';
 
-const ChatInput: React.FC = () => {
+export interface ChatInputProps {
+    submitHandler: (event: React.FormEvent) => void
+    onChangeHandler: (event: React.ChangeEvent<HTMLInputElement>) => void
+    value: string
+}
+
+export default function ChatInput({ handlers }: { handlers: ChatInputProps }) {
     return (
         <div className={styles.chatSection}>
-            <input className={styles.messageInput} type="text" placeholder="Message general" />
+            <form onSubmit={handlers.submitHandler}>
+                <input 
+                    className={styles.messageInput}
+                    type="text"
+                    placeholder="Message general"
+                    value={handlers.value}
+                    onChange={handlers.onChangeHandler}
+                />
+                <button type="submit" className="bg-blue-700 rounded-lg text-sm py-2 mb-2 px-3 me-2">Send message</button>
+            </form>
         </div>
-    );
-};
-
-export default ChatInput;
+    )
+}
