05eb72752d3bb88e94d81ffba4c81f0bed323fdb
Arberim Ame <amea@oregonstate.edu>
2024-11-08 21:41:09 -0500

    Hit the backend API and retrieve previous messages to display in Chat component

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,12 @@ import React from 'react';
 import Chat from "@/components/Chat";
 import { createClient } from '@/utils/supabase/server'
 import { redirect } from 'next/navigation'
+import { MessageProps, ChannelMessage } from '@/utils/types/types'
+
+/**
+ *  The URL to the backend REST API
+ */
+const api: string = process.env.BACKEND_API!
 
 const ChatPage: React.FC = async () => {
   // Check if user is authenticated
@@ -14,9 +20,34 @@ const ChatPage: React.FC = async () => {
   if (!user) {
       return redirect('/login')
   }
+
+  // fetch previous messages
+  // for now hardcoding general
+  let previous: MessageProps[] = []
+  try {
+    const response = await fetch(api + '/channels/4/messages', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+
+    if (response.ok) {
+      const data = await response.json()
+      // Map the differences between api and socket formats
+      previous = data.map((message: ChannelMessage) => ({
+        user: message.users.display_name,
+        body: message.body,
+        timestamp: message.created_at
+      }))
+    }
+  } catch (error) {
+    console.log('Failed to receive general chat messages: ', error)
+    redirect('/error')
+  }
+  
   return (
-    <Chat user={user} />
-    
+    <Chat user={user} previousMessages={previous} />
   );
 };
 
