b384053eb5efe636338f06f74bfbc6b91f8c077a
Arberim Ame <amea@oregonstate.edu>
2024-11-14 03:10:23 -0500

    Format ISO string to locale string

--- components/Message.tsx
+++ components/Message.tsx
@@ -6,9 +6,12 @@ import styles from '@/app/ChatPage.module.css';
 import { MessageProps } from '@/utils/types/types'
 
 const Message: React.FC<MessageProps> = ({ user, body, timestamp }) => {
+  
+  const date = new Date(timestamp)
+  const formattedDate = date.toLocaleString()
   return (
     <div className={styles.message}>
-      <span className={styles.user}>{user}</span> <span className={styles.timestamp}>{timestamp}</span>
+      <span className={styles.user}>{user}</span> <span className={styles.timestamp}>{formattedDate}</span>
       <p>{body}</p>
     </div>
   );
