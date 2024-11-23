841fce73a2bf1a4c29c3b777ae7c91f2b50f8f6d
Scottie Thompson <scottiecodes@gmail.com>
2024-11-22 23:56:04 -0500

    Updated NavBar to use icons/scaling

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,23 +1,20 @@
-/**
- * Navigation bar, applied to default page layout
- */
+import React from 'react';
+import { AiOutlineHome, AiOutlineSetting } from 'react-icons/ai'; // Icons for home and admin
+import styles from './ChatPage.module.css'; // Assuming CSS module for styling
 
-"use client"
-
-import { NavItem } from '@/components/NavItem'
-
-export function NavBar() {
+const NavBar = () => {
     return (
-        <nav className="bg-white border-gray-200 dark:bg-gray-800">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Capstone</span>
-                <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-800 dark:border-gray-700">
-                        <li><NavItem href="/">Home</NavItem></li>
-                        <li><NavItem href="/AdminPanel">Admin</NavItem></li>
-                    </ul>
-                </div>
-            </div>
+        <nav className={styles.navbar}>
+            <ul className={styles.navItems}>
+                <li className={`${styles.navItem} ${styles.active}`}>
+                    <AiOutlineHome size={24} />
+                </li>
+                <li className={styles.navItem}>
+                    <AiOutlineSetting size={24} />
+                </li>
+            </ul>
         </nav>
-    )
-}
+    );
+};
+
+export default NavBar;
