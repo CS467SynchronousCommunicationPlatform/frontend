ec8c040e8742811440dc701330007ce308cf1e8b
Scottie Thompson <scottiecodes@gmail.com>
2024-11-23 00:06:40 -0500

    Updated NavBar to use icons/scaling

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { AiOutlineHome, AiOutlineSetting } from 'react-icons/ai'; // Icons for home and admin
 import styles from '@/app/ChatPage.module.css'
 import {NavItem} from "@/components/NavItem";
+import { useRouter } from 'next/router';
 
 const NavBar = () => {
+    const router = useRouter(); // current route
+
     return (
         <nav className={styles.navbar}>
             <ul className={styles.navItems}>
-                <li className={`${styles.navItem} ${styles.active}`}>
+                <li className={`${styles.navItem} ${router.pathname === '/' ? styles.active : ''}`}>
                     <NavItem href="/"><AiOutlineHome size={24} /></NavItem>
                 </li>
-                <li className={styles.navItem}>
+                <li className={`${styles.navItem} ${router.pathname === '/' ? styles.active : ''}`}>
                     <NavItem href="/admin"><AiOutlineSetting size={24} /></NavItem>
                 </li>
             </ul>
