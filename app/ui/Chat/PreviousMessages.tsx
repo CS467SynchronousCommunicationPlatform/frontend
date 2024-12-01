979cbb94dad7ffeda171740d52449c03b19a064c
Blake Hakkila <hakkilab@oregonstate.edu>
2024-12-01 03:21:38 -0800

    Updated previous message user name when display name is updated

--- app/ui/Chat/PreviousMessages.tsx
+++ app/ui/Chat/PreviousMessages.tsx
@@ -1,18 +1,49 @@
 /**
  * Represents the previous messages in a channel
  */
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { socket } from '@/socket';
 import Message from '@/app/ui/Chat/Message';
 import { MessageProps } from '@/app/lib/types/types';
 import { useAppState } from '@/app/lib/contexts/AppContext';
 
 export default function PreviousMessages() {
-    const { state } = useAppState();
+    const { state, dispatch } = useAppState();
     const { currentChannel, allMessages } = state;
 
     const messages = allMessages.get(currentChannel) || [];
 
     const bottomRef = useRef<HTMLDivElement | null>(null);
+
+    const [isConnected, setIsConnected] = useState(socket.connected);
+
+    useEffect(() => {
+        const onConnect = () => setIsConnected(true);
+        const onDisconnect = () => setIsConnected(false);
+
+        socket.on('connect', onConnect);
+        socket.on('disconnect', onDisconnect);
+
+        socket.on('displayname', async (socketMessage) => {
+            // create updated message listing and set it
+            const newMessages = new Map<number, MessageProps[]>;
+            for (const [channelId, messages] of allMessages.entries()) {
+                let messageCopy = [...messages]
+                for (let message of messageCopy) {
+                    if (message.user == socketMessage.previous) {
+                        message.user = socketMessage.new;
+                    }
+                }
+                newMessages.set(channelId, messageCopy);
+            }
+            dispatch({ type: 'SET_MESSAGES', payload: newMessages });
+        })
+
+        return () => {
+            socket.off('connect', onConnect);
+            socket.off('disconnect', onDisconnect);
+        };
+    }, []);
 
     useEffect(() => {
         bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
