8472705a7bfdac0a271dcc8e3e078b1d9e8decb3
Blake Hakkila <hakkilab@oregonstate.edu>
2024-11-30 23:56:15 -0800

    Fixed admin panel user name display

--- app/AdminPanel/AdminPanel.tsx
+++ app/AdminPanel/AdminPanel.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import React, { useState } from 'react';
-import { updateUserDisplayName } from '@/app/lib/api/api';
+import React, { useState, useEffect } from 'react';
+import { updateUserDisplayName, fetchAllUsers } from '@/app/lib/api/api';
 import { Heading } from "@/app/ui/Catalyst/heading";
 import { Divider } from "@/app/ui/Catalyst/divider";
 import { Text } from "@/app/ui/Catalyst/text";
@@ -11,6 +11,18 @@ const AdminPanelComponent: React.FC = () => {
     const { state } = useAppState();
     const [displayName, setDisplayName] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
+    const [userName, setUserName] = useState('');
+
+    useEffect(() => {
+        fetchAllUsers().then(users => {
+            for (const u of users) {
+                if (u.id == state.user?.id) {
+                    console.log(u)
+                    setUserName(u.display_name);
+                }
+            }
+        });
+    }, []);
 
     const handleSubmit = async (event: React.FormEvent) => {
         event.preventDefault();
@@ -29,6 +41,7 @@ const AdminPanelComponent: React.FC = () => {
 
             if (data) {
                 console.log('Display name updated:', data);
+                setUserName(data[0].display_name);
             } else {
                 setErrorMessage('Failed to update display name');
             }
@@ -43,7 +56,7 @@ const AdminPanelComponent: React.FC = () => {
         <div>
             <Heading>Admin Panel</Heading>
             <Divider className={'my-6'}></Divider>
-            <Text>Welcome to the admin panel, {state.user?.user_metadata.displayName}</Text>
+            <Text>Welcome to the admin panel, {userName}</Text>
             <form onSubmit={handleSubmit}>
                 <label>
                     Display Name:
